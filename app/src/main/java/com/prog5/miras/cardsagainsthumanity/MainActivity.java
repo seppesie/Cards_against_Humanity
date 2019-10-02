76800ceff8894bcb310679b5b18c4b8d3f6051ad
Seppe De waele <r0784020@student.vives.be>
2019-10-02 14:20:12 +0200

    Initial commit

---
+++ app/src/main/java/com/prog5/miras/cardsagainsthumanity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prog5.miras.cardsagainsthumanity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
